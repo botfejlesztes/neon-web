5d0f853a1f6df062122fea9be475302dac8915df
jozsoka2222 <36086735+jozsoka2222@users.noreply.github.com>
2020-09-21 19:23:20 +0200

    version 1.0

---
+++ build/precache-manifest.c52dcaca886fc1f0bbb15acc0a9768a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120b0410e0b367b6c8c9e739f1945187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3629ab9941885fe2706",
+    "url": "/static/css/2.373572e1.chunk.css"
+  },
+  {
+    "revision": "a5cd4af5cedd5d16898b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f3629ab9941885fe2706",
+    "url": "/static/js/2.9e8e1648.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9e8e1648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5cd4af5cedd5d16898b",
+    "url": "/static/js/main.6f1c9b38.chunk.js"
+  },
+  {
+    "revision": "0fbe23201c0f05c8a3ad",
+    "url": "/static/js/runtime-main.afad2166.js"
+  }
+]);
